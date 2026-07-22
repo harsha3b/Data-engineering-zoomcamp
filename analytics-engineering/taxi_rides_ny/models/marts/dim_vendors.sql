fe031a5a5e54e88291bab5459c9cdc57dc134880
Harshavardhanreddy Bayyapureddy <harsha3b@gmail.com>
2026-07-22 13:27:14 +0000

    vendor detail model created along with a simple macro

--- analytics-engineering/taxi_rides_ny/models/marts/dim_vendors.sql
+++ analytics-engineering/taxi_rides_ny/models/marts/dim_vendors.sql
@@ -1,1 +1,9 @@
-select 1
+with vendors as (
+    select distinct vendor_id
+    from {{ ref('int_trips_2019_unioned') }}
+)
+
+select
+    vendor_id,
+    {{get_vendor_names('vendor_id')}} as vendor_name --macro for case statement
+from vendors
