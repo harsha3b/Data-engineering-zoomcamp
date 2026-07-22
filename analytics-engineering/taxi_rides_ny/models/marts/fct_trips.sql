b102561790e23ad1e7e8954e7758c15a7e23a58b
Harshavardhanreddy Bayyapureddy <harsha3b@gmail.com>
2026-07-22 18:42:41 +0000

    fact table model created

--- analytics-engineering/taxi_rides_ny/models/marts/fct_trips.sql
+++ analytics-engineering/taxi_rides_ny/models/marts/fct_trips.sql
@@ -1,1 +1,26 @@
-select 1
+with fct_table as (
+    select
+        {{ dbt_utils.generate_surrogate_key([
+            'vendor_id',
+            'pickup_datetime',
+            'dropoff_datetime',
+            'pickup_location_id',
+            'dropoff_location_id'
+        ]) }} as trip_id,
+        *
+    from {{ ref('int_trips_2019_unioned') }}
+    qualify row_number() over (
+        partition by vendor_id, pickup_datetime, dropoff_datetime, pickup_location_id, dropoff_location_id
+        order by pickup_datetime
+    ) = 1
+),
+payment_type_lookup as (
+    select * from {{ ref('payment_type_lookup') }}
+)
+
+select
+    f.* except (payment_type),
+    p.description as payment_type
+from fct_table f
+left join payment_type_lookup p
+    on f.payment_type = p.payment_type
